a158be2286f9edc465fc3f8bbb2b9b7df5789104
Suveer Keshav <skeshav@k12.com>
2019-02-22 23:41:18 -0500

    Implemented api endpoints and initialiazed webshot.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use 'esversion: 6'";
 //Implement your backend in this file
 
 //Do not change these imports
@@ -9,12 +10,118 @@ var app = express();
 app.use(express.json());
 var multer = require('multer');
 var upload = multer({dest: "uploads/"});
+var path = require("path");
+var mongoose = require('mongoose'); 
+var webshot = require('webshot');
+ 
+
+
+// Connect to MongoDB
+const mongoDbBaseUrl = 'mongodb://localhost:27017/RegressTest';
+testDB = mongoose.connect(mongoDbBaseUrl, {useNewUrlParser: true});
+
+const Test = mongoose.model('skeshav', { name: String });
 
 
 //Implementation should be below
+
+// Save test case
+app.post('/api/savetestcase', function (req, res) {
+
+    const testCaseName = Object.keys(req.body)[0];
+    const testCaseBody = req.body[testCaseName];
+    if( testCaseBody && (typeof testCaseBody == "object") ){
+        var options = {
+            onLoadFinished: function() {
+              var links = document.getElementsByTagName('a');
+           
+              for (var i=0; i<links.length; i++) {
+                var link = links[i];
+                link.innerHTML = 'My custom text';
+              } 
+            }
+        };
+        
+        var _res = res;
+        
+        webshot('google.com', 'google.png', options, function(err) {
+            // screenshot now saved to google.png
+            _res.status(200).send({"message": "SUCCESS"});
+        });
+        //testCaseBody
+        const pingTest = new Test(testCaseBody);
+        pingTest.save().then(() => {
+            res.status(200).send({status: "SUCCESS", message: "Test Case Saved"});
+        }, ()=>{
+            res.status(500).send({status: "ERROR", message: "Internal Error: Could not save to Mongo DB"});
+        });
+        
+    }else{
+        res.status(400).send({status: "ERROR", message: "Bad input"});
+    }
+
+    
+
+});
+
+// Get all test cases
+app.get('/api/testcases', function (req, res) {
+    // use mongoose to get all testcases in the database
+    Test.find(function(err, testCases) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err);
+
+        res.status(200).json(testCases); // return all tests in JSON format
+    });
+
+});
+
+
+// Get test case for test_id
+app.get('/api/testcase/:test_id', function (req, res) {
+    // use mongoose to get a testcase with _id:test_id in the database
+    Test.find({_id: req.params.test_id},function(err, testCase) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err);
+
+        res.status(200).json(testCase); // return test in JSON format
+    });
+
+});
+
+// Run test case with _id: test_id
+app.get('/api/runtestcase/:test_id', function (req, res) {
+    var options = {
+        onLoadFinished: function() {
+          var links = document.getElementsByTagName('a');
+       
+          for (var i=0; i<links.length; i++) {
+            var link = links[i];
+            link.innerHTML = 'My custom text';
+          } 
+        }
+    };
+    
+    var _res = res;
+    
+    webshot('google.com', 'google.png', options, function(err) {
+        // screenshot now saved to google.png
+        _res.status(200).send({"message": "SUCCESS"});
+    });
+
+});
+
+
+
+
+
 // application -------------------------------------------------------------
 app.get('*', function(req, res) {
-    res.sendfile('./www/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(path.join(__dirname, 'www/index.html')); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.port || 9977;
 const app = require('./app');
 
 app.listen(port, function () {
-    console.log('Example app listening on port ' + port);
+    console.log('Regress app listening on port ' + port);
 });
