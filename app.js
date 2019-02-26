53ef56f144d587b6eaeaf89405f2e51ddf5dc8b3
Suveer Keshav <skeshav@k12.com>
2019-02-26 12:43:58 -0500

    Checking in a sample test case to show that pixelmatch diff works.

--- app.js
+++ app.js
@@ -40,15 +40,35 @@ app.post('/api/savetestcase', function (req, res) {
     const testCaseBody = req.body[testCaseName];
     if( testCaseBody && (typeof testCaseBody == "object") ){
         var func = new Function("return " + "function (a, b) { alert(a+b); return a + b; }")();
+        var _console = console;
+
         var options = {
             renderDelay: 15000,
-            onLoadFinished: function() {
-                // var searchInputField = $('.fan-input').get(0);
-                // searchInputField.value = "wonder woman";
-                // searchInputField.dispatchEvent(new Event("input"));
-                // $('.fan-btn.fan-btn-style-go').click();
-                
+            errorIfJSException: true,
+            errorIfStatusIsNot200: true,
+            onError : {
+                fn: function(msg, trace) {
+                    var msgStack = ['ERROR: ' + msg];
+                    if (trace && trace.length) {
+                        msgStack.push('TRACE:');
+                        trace.forEach(function(t) {
+                            msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+                        });
+                    }
+                    system.stderr.write(msgStack.join('\n'));
+                }, context: {logger: _console.error}
+            },
+            onLoadFinished: {
+                fn: function(status) {
+                    system.stderr.write('Status: ' + status);
+                    // var searchInputField = $('.fan-input').get(0);
+                    // searchInputField.value = "wonder woman";
+                    // searchInputField.dispatchEvent(new Event("input"));
+                    // $('body').css("padding-left", 10);
+                    system.stderr.write("OK");
+                    
 
+                }, context: {logger: _console.log}
             }
         };
         
@@ -60,11 +80,13 @@ app.post('/api/savetestcase', function (req, res) {
         let name = testCaseBody.name;
         let refImagePath = path.join(__dirname, 'reference-images/'+name+'_ref.png');
         
-        webshot('https://www.w3schools.com/', refImagePath, options, function(err) {
-        // webshot(testCaseUrl, refImagePath, options, function(err) {
+        // webshot('https://www.w3schools.com/', refImagePath, options, function(err) {
+        webshot(testCaseUrl, refImagePath, options, function(err) {
             // screenshot now saved to the specified path
-            //_res.status(200).send({"message": "SUCCESS"});
-
+            if(err){
+                _console.log(err);
+            }
+            _console.log("OK");
             //testCaseBody
             const testCase = new Test(_testCaseBody);
             testCase.image.data = _fs.readFileSync(refImagePath);
@@ -142,30 +164,50 @@ app.get('/api/runtestcase/:test_id', function (req, res) {
         let testCaseUrl = testCase.url;
         let name = testCase.name;
         let contentType = testCase.image.contentType;
-        let refImagePath = path.join(__dirname, 'reference-images/'+name+'_ref.png');
         let outputImagePath = path.join(__dirname, 'outputs/'+name+'_testoutput.png');
         let diffImagePath = path.join(__dirname, 'diffs/'+name+'_diff.png');
+        var refData = testCase.image.data;
+        
+        /* In case we want to write the image to a png file on disk, the below commented lines are checked in  */
+        // let refImagePath = path.join(__dirname, 'reference-images/'+name+'_ref.png');
+        // var buf = Buffer.from(refData, 'base64');
+        // fs.writeFile(refImagePath, buf, function(err) {
+        //     if (err) throw err;
+        // });
 
-        var data = testCase.image.data;
-        var buf = Buffer.from(data, 'base64');
-        fs.writeFile(refImagePath, buf, function(err) {
-            if (err) throw err;
-        });
+        var _console = console;
 
         var options = {
             renderDelay: 15000,
-            onLoadFinished: function() {
-                // var searchInputField = $('.fan-input').get(0);
-                // searchInputField.value = "wonder woman";
-                // searchInputField.dispatchEvent(new Event("input"));
-                // $('body').css("padding-left", 10);
-                setTimeout(function() {
-                    var links = document.getElementsByTagName('a');
-                    links[0].innerHTML = '';
-                }, 5000);
-                
+            errorIfJSException: true,
+            errorIfStatusIsNot200: true,
+            onError : {
+                fn: function(msg, trace) {
+                    var msgStack = ['ERROR: ' + msg];
+                    if (trace && trace.length) {
+                        msgStack.push('TRACE:');
+                        trace.forEach(function(t) {
+                            msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+                        });
+                    }
+                    this.logger(msgStack.join('\n'));
+                }, context: {logger: _console.error}
+            },
+            onLoadFinished: {
+                fn: function(status) {
+                    this.logger('Status: ' + status);
+                    // var searchInputField = $('.fan-input').get(0);
+                    // searchInputField.value = "wonder woman";
+                    // searchInputField.dispatchEvent(new Event("input"));
+                    // $('body').css("padding-left", 10);
+                    setTimeout(function() {
+                        var links = document.getElementsByTagName('a');
+                        links[0].innerHTML = '';
+                    }, 5000);
+                    this.logger("OK");
+                    
 
-
+                }, context: {logger: _console.log}
             }
         };
         
@@ -174,12 +216,14 @@ app.get('/api/runtestcase/:test_id', function (req, res) {
         var _PNG = PNG;
         var _pixelmatch = pixelmatch;
         
-        webshot('https://www.w3schools.com/', outputImagePath, options, function(err) {
-        // webshot(testCaseUrl, outputImagePath, options, function(err) {
+        // webshot('https://www.w3schools.com/', outputImagePath, options, function(err) {
+        webshot(testCaseUrl, outputImagePath, options, function(err) {
+            if (err){
+                _console.log(err);
+            }
             // screenshot now saved to <name>_testoutput.png
             var testData = _fs.readFileSync(outputImagePath);
             var testImg = _PNG.sync.read(testData);
-            var refData = data;
             var refImg = _PNG.sync.read(refData);
 
             if((testImg.height != refImg.height) || (testImg.width != refImg.width)){
